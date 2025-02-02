8e69a4e7af61c054f1e1dec5f07211b14200d121
jerzy szajner <jerzy.szajner@gmail.com>
2025-02-02 18:54:58 +0100

    Add navigation.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
 import fetchFilms from "./fetchFilms.js";
+import handleNavbarButtons from "./navigation.js";
 
 // Start App
 const startApp = () => {
   console.log("🚀 App started...");
   fetchFilms();
+  handleNavbarButtons();
 };
 
 document.addEventListener("DOMContentLoaded", startApp);
---
+++ src/js/navigation.js
@@ -0,0 +1,71 @@
+import fetchFilms from "./fetchFilms";
+
+const navbar = document.querySelector(".navbar");
+const navbarItems = document.querySelectorAll(".navbar__item");
+const activeFilms = document.querySelector('[data-action="films"]');
+const navbarControlsList = document.querySelector(".navbar__controls-list");
+const cardsSection = document.querySelector(".card__section");
+
+// Adjust navbar for mobile
+const adjustNavbarForMobile = () => {
+  const activeItem = document.querySelector(".navbar__item--active");
+
+  if (window.innerWidth < 600) {
+    activeItem.classList.remove("navbar__item--active");
+    cardsSection.style.display = "none";
+  }
+};
+
+adjustNavbarForMobile();
+
+// Handle navbar buttons
+const handleNavbarButtons = (e) => {
+  const clickedButton = e.target.closest("button");
+
+  if (!clickedButton || !clickedButton.dataset.action) return;
+  e.preventDefault();
+
+  const parentItem = clickedButton.closest(".navbar__item");
+  if (!parentItem) return;
+
+  navbarItems.forEach((item) => {
+    item.classList.remove("navbar__item--active");
+  });
+
+  parentItem.classList.add("navbar__item--active");
+  const action = clickedButton.dataset.action;
+  navbarControlsList.classList.remove("navbar__controls-list--active");
+
+  // Handle actions
+  switch (action) {
+    case "logo":
+      fetchFilms();
+      cardsSection.style.display = "grid";
+      break;
+    case "toggle":
+      navbarControlsList.classList.add("navbar__controls-list--active");
+      setTimeout(() => {
+        cardsSection.style.display = "none";
+      }, 800);
+      break;
+    case "films":
+      fetchFilms();
+      cardsSection.style.display = "grid";
+      console.log("Films button clicked");
+      break;
+    case "people":
+      console.log("People button clicked");
+      break;
+    case "planets":
+      console.log("Planets button clicked");
+      break;
+    case "vehicles":
+      console.log("Vehicles button clicked");
+      break;
+  }
+};
+
+// Event listener
+navbar.addEventListener("click", handleNavbarButtons);
+
+export default handleNavbarButtons;
